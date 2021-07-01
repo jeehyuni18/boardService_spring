bc8890f01e8d7b2e6c9a9e6eb4bcbbc8065f4622
jeehyuni18 <jeehyuni18@gmail.com>
2021-07-02 05:38:07 +0900

    first commit

---
+++ src/main/java/com/sparta/week03/domain/Memo.java
@@ -0,0 +1,50 @@
+package com.sparta.week03.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor // 기본생성자를 만듭니다.
+@Getter
+@Entity // 테이블과 연계됨을 스프링에게 알려줍니다.
+public class Memo extends Timestamped { // 생성,수정 시간을 자동으로 만들어줍니다.
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+
+
+    public Memo(Long id, String username, String title, String contents ) {
+        this.id=id;
+        this.username = username;
+        this.title=title;
+        this.contents = contents;
+
+    }
+
+    public Memo(MemoRequestDto requestDto) {
+        this.id=requestDto.getId();
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+        this.title=requestDto.getTitle();
+    }
+
+    public void update(MemoRequestDto requestDto) {
+        this.id=requestDto.getId();
+        this.username = requestDto.getUsername();
+        this.title=requestDto.getTitle();
+        this.contents = requestDto.getContents();
+
+    }
+}
